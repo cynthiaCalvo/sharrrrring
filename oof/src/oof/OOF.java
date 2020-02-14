e93b72167a03b11fa59715ceff1e8e5d6ded08c4
David <Davidsawatzky2002@gmail.com>
2020-02-14 11:57:10 -0600

    Added cow int

--- oof/src/oof/OOF.java
+++ oof/src/oof/OOF.java
@@ -3,9 +3,13 @@ package oof;
 public class OOF {
 
 	public static void main(String[] args) {
+		int cow = 4;
+		
 		System.out.println("HI");
 		System.out.println("help"); //something else
 		//HIHIHIHI
+		
+		
 
 	}
 
