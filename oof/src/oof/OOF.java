6900d84b80701647420daf946398b29a3f86726e
David <Davidsawatzky2002@gmail.com>
2020-02-14 12:03:46 -0600

    Added an if statement

--- oof/src/oof/OOF.java
+++ oof/src/oof/OOF.java
@@ -5,6 +5,10 @@ public class OOF {
 	public static void main(String[] args) {
 		int cow = 4, moo = 2;
 		
+		if(cow > moo) {
+			System.out.print("hi");
+		}
+		
 		System.out.println("HI");
 		System.out.println("help"); //something else
 		//HIHIHIHI
